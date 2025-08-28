c7e888571ca88f08355e20296776f8fb9c78eb48
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-08-28 22:43:10 +0200

    es. 2

---
+++ hobbies.js
@@ -0,0 +1,5 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+  return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+export default hobbies;
--- main.js
+++ main.js
@@ -2,3 +2,9 @@ import nomi from "./name.js";
 
 console.log(nomi("Maria", "Rossi"));
 console.log(nomi("Marco", "Bianchi"));
+
+import hobbies from "./hobbies.js";
+
+console.log(
+  hobbies("giocare ai videogiochi", "giocare a basket", "programmare")
+);
